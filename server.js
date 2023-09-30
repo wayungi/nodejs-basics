be71f2abff659be9ff4073e675dd7585b0837cc1
wayungi <fwayungi.com>
2023-09-30 13:48:18 +0300

    UPDATE: catchall to respond according to request type

--- server.js
+++ server.js
@@ -50,7 +50,16 @@ app.get('^/$|oldpage(.html)?', (req, res) => { //oldpage does not exists
 
 // we can update the catch all route like abelow
 app.all('*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+
+    res.status(404);
+
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }else if(req.accepts('json')){
+        res.json({error: "404 Not Found"});
+    }else {
+        res.type('txt').send('404 Not found');
+    }
 })
 
 
