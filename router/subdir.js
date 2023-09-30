c81351d8ab3eb5c01e74ca2e6515880ba5abfac0
wayungi <fwayungi.com>
2023-09-30 14:04:37 +0300

    ADD: subdir routes

--- router/subdir.js
+++ router/subdir.js
@@ -3,5 +3,13 @@ const router =  require('router');
 const path = require('path');
 
 
+router.get('^/$|index(.html)?', (req, res) => { 
+    res.sendFile(path.join(__dirname, '..', 'views', 'subdir','index.html'));
+});
+
+router.get('^/$|test().html)?', (req, res) => { 
+    res.sendFile(path.join(__dirname, '..', 'views', 'subdir','test.html'));
+});
+
 
 module.exports = router;
