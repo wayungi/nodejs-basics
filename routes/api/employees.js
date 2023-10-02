e05a95198943b0c4fe70a918babbf28c8bdc8a51
wayungi <fwayungi.com>
2023-10-02 23:55:35 +0300

    ADD: array to store data in memory

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const data = {};
-data.employees =  require('../../data/employees.json');
+// data.employees =  require('../../data/employees.json');
+data.employees =  [];
+const {v4: uuid} = require('uuid');
+
 
 
 //INTERSTING: params (in the url) vs body = (in the json body)
@@ -12,16 +15,30 @@ router.route('/')
         res.json(data.employees);
     })
     .post((req, res) => {
-        res.json({
+        const user = {
+            "id": req.body.id,
             'firstname': req.body.firstname,
             'lastname': req.body.lastname
-        })
+        }
+        data.employees.push(user);
+        res.json(user)
     })
+
     .put((req, res) => {
-        res.json({
+        const updatedUser = {
+            'id': req.body.id,
             'firstname': req.body.firstname,
             'lastname': req.body.lastname
-        })
+        }
+        
+        let user = data.employees.filter((item) => item.id === req.body.id);
+        if(user){
+            user = updatedUser;
+            res.json(updatedUser);
+        }else {
+            res.json({"error": "user not found"})
+        }
+        
     })
     .delete((req, res) => {
         res.json({"id": req.body.id})
