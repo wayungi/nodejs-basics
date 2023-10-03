5107d543e511ede3006b49baaa5b3b0db8b605de
wayungi <fwayungi.com>
2023-10-03 12:57:57 +0300

    FIX: controller method calls

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,9 +1,10 @@
-const {v4: uuid} = require('uuid');
 const data = {
     employees: require('../model/employees.json'),
     setEmployees: function(data){this.employees = data}
    };
 
+
+   
 const getAllEmployees = (req, res) => {
     res.json(data.employees);
 }
--- routes/api/employees.js
+++ routes/api/employees.js
@@ -6,10 +6,10 @@ const employeeController =  require('../../controllers/employeesController');
 //INTERSTING: params (in the url) vs body = (in the json body)
 
 router.route('/')
-    .get(employeeController.getAllEmployees())
-    .post(employeeController.createNewEmployee())
-    .put(employeeController.updateEmployee())
-    .delete(employeeController.deleteEmployee());
+    .get(employeeController.getAllEmployees)
+    .post(employeeController.createNewEmployee)
+    .put(employeeController.updateEmployee)
+    .delete(employeeController.deleteEmployee);
 router.route('/:id')
-    .get(employeeController.getEmployee());
+    .get(employeeController.getEmployee);
 module.exports = router;
