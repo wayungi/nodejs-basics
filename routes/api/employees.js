bfe01145cf58e8a844f2c819ec2b6663c943bb2e
wayungi <fwayungi.com>
2023-10-03 10:58:09 +0300

    ADD: EmployeesController method exports

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -6,7 +6,6 @@ const data = {};
 //INTERSTING: params (in the url) vs body = (in the json body)
 
 router.route('/')
-
     .get(getAllEmployees())
     .post(createNewEmployee())
     .put(updateEmployee())
