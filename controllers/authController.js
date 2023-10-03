cfb33a0a969bb77026398424ce7e1dac69b4de5a
wayungi <fwayungi.com>
2023-10-03 22:20:08 +0300

    ADD: login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,3 +4,29 @@ const usersDB = {
         this.users = (data);
     }
 }
+
+const path = require('path');
+const bcrypt = require('bcrypt');
+const fsPromises = require('fs').promises;
+
+const handleLogin = async(req, res) => {
+    const {user, pwd} = req.body;
+    if(!user || !pwd) return res.status(400).json({'message': 'username & password are required'});
+
+    //find user by username
+    const foundUser = usersDB.users.find((person) => person.username === user);
+    if(!foundUser) return res.sendStatus(401) //Unauthorized
+
+    //evaluate password
+    const match = bcrypt.compare(pwd, foundUser.password)
+    if(match){
+        res.status(200).json({"user": foundUser})
+    }else{
+        res.sendStatus(401)
+    }
+
+
+}
+
+module.exports = {handleLogin}
+
