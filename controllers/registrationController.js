ab605893d84a22e1fffaefc7b28e0428cc464904
wayungi <fwayungi.com>
2023-10-03 21:10:31 +0300

    ADD: newUser registration

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -4,3 +4,28 @@ const usersDB = {
         this.users = (data);
     }
 }
+
+const path = require('path');
+const bcrypr = require('bcrypt');
+const fsPromises = require('fs').promises;
+
+const handleNewUser = async(req, res) => {
+    const {user, pwd} = req.body;
+    if(!user || !pwd) return res.status(400).json({'message': 'username & password are required'})
+
+    //check fof duplicate entry
+    const duplicate = usersDB.users.find((person) => person.username === user);
+    if(duplicate){
+        return res.status(409).json({"error": "name already exists"})
+    }
+    try{
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        const newUser = {username: user, password: hashedPwd}
+        setUsers([...usersDB.users, newUser])
+        //write the data to file
+        await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(usersDB.users))
+        res.status(201).json({"result": newUser});
+    }catch(err){
+        return res.status(500).json({"error": err.message})
+    }
+}
